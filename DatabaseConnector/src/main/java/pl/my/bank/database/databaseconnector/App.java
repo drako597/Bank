4761a9b86fb2b470d6252f5ae856542ee7f1d3c5
karol <karol@karol-laptop>
2014-02-27 15:53:50 +0100

    nowy moduł databaseconnector

---
+++ DatabaseConnector/src/main/java/pl/my/bank/database/databaseconnector/App.java
@@ -0,0 +1,13 @@
+package pl.my.bank.database.databaseconnector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
